d2896bafae9186614e2bf047b8993ab555099235
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-02-09 13:41:04 -0500

    feat: day 1 part two solution :tada:

--- src/one.ts
+++ src/one.ts
@@ -12,3 +12,20 @@ export const one = (input: string): number => {
   totalList.sort((a: number, b: number) => a - b);
   return totalList[totalList.length - 1];
 };
+
+export const one_partTwo = (input: string): number => {
+  const toList = input.split(`\n`);
+  let totalList: number[] = [];
+
+  toList.reduce((acc: number, curr: string): number => {
+    if (isNaN(parseInt(curr))) {
+      totalList.push(acc);
+      return 0;
+    }
+    return acc + parseInt(curr);
+  }, 0);
+  totalList.sort((a: number, b: number) => a - b);
+  return totalList
+    .splice(totalList.length - 3, 3)
+    .reduce((acc: number, curr: number): number => acc + curr, 0);
+};
